2233a8ca33cd4add6f4618abb068e94fa8bc2d13
Guneet Singh <guneetnagia24@gmail.com>
2020-07-03 17:37:35 +0530

    jenkins test'

--- src/com/java8/javabypatel/StreamTest.java
+++ src/com/java8/javabypatel/StreamTest.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class StreamTest {
 	public static void main(String aaa[]){
-		System.out.println("start");
+		System.out.println("starting");
 		Student student1 = new Student("Jayesh", 20, new Address(410014L), Arrays.asList(new MobileNumber(9090909090L), new MobileNumber(8080808080L)));
 		Student student2 = new Student("Khyati", 20, new Address(1235L), Arrays.asList(new MobileNumber(7777777777L), new MobileNumber(666666666L)));
 		Student student3 = new Student("Jason", 20, new Address(3333L), Arrays.asList(new MobileNumber(123212345L), new MobileNumber(556666666L)));
