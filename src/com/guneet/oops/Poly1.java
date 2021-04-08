c32b62d892be859cb9aceadb56ce11b392bac73b
Guneet Singh <guneetnagia24@gmail.com>
2021-04-08 17:37:37 +0530

    test

--- src/com/guneet/oops/Poly1.java
+++ src/com/guneet/oops/Poly1.java
@@ -1,18 +1,19 @@
 package com.guneet.oops;
 
 interface A{
-	int a = 100;
+	static final int a = 100;
 	public void display();
 }
 
 interface B{
-	int a = 100;
+	static final int a = 100;
 	public void display();
 }
 
 public class Poly1 implements A,B{
 	public static void main(String args[]){
-		//System.out.println(a);   //error
+		
+	//	System.out.println(a);   //error
 	}
 
 	@Override
