4cbe695af1a9b07c48fe80cfd28a5eb069b9a010
Guneet Singh Nagia <guneetnagia@users.noreply.github.com>
2023-12-28 01:05:33 +0530

    Update Main.java

--- src/datastructures/heaps/Main.java
+++ src/datastructures/heaps/Main.java
@@ -10,5 +10,20 @@ public class Main {
         System.out.println(heap.getHeap());
         heap.insert(100);
         System.out.println(heap.getHeap());
+        heap.insert(75);
+        System.out.println(heap.getHeap());
+        Heap heap2 = new Heap();
+        heap2.insert(95);
+        heap2.insert(75);
+        heap2.insert(80);
+        heap2.insert(55);
+        heap2.insert(60);
+        heap2.insert(50);
+        heap2.insert(65);
+        System.out.println(heap2.getHeap()); // 95, 75, 80, 55, 60, 50, 65
+        heap2.removes();
+        System.out.println(heap2.getHeap()); // 80, 75, 65, 55, 60, 50
+        heap2.removes();
+        System.out.println(heap2.getHeap()); // 75, 60, 65, 55, 50
     }
 }
